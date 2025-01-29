c7ec74c80ea8b8d57ad94beb547e778eb7ccb9b2
mataager <omarbussmo077@gmail.com>
2025-01-29 21:37:32 +0200

    Add files via upload

---
+++ assets/js/static.js
@@ -0,0 +1,54 @@
+const url = "https://matager-f1f00-default-rtdb.firebaseio.com";
+const storeurl = "https://matager-f1f00-default-rtdb.firebaseio.com";
+const storename = "KadryStore";
+const uid = "MoBAXZuhD1X2MOsRF2qfQuoWAUD2";
+const storeuid = "MoBAXZuhD1X2MOsRF2qfQuoWAUD2";
+const storePhoneNum = "+20 114 2077 658 ";
+const storePhoneNum2 = "";
+const storeEmail = "Kadrystore2025@gmail.com";
+const storeGender = ["Men", "Women", "Kids"];
+const storeType = "clothing"; // clothing or footwear
+const freeshipping = "2000";
+//images url
+const pannerurl =
+  "https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/d9d131120670167.60b65a57b81b3.png";
+//
+const mentopurl = "";
+const menbottomurl = "";
+const menfootwareurl = "";
+const menshoseseurl = "";
+const menslippersurl = "";
+const menflipflopsurl = "";
+const mensandalsurl = "";
+const menLoafersurl = "";
+const menMoccasinsurl = "";
+const menclassicshoseurl = "";
+const menbootsurl = "";
+//
+const womentopurl = "";
+const womenbottomurl = "";
+const womenfootwareurl = "";
+const womenshoseseurl = "";
+const womenheelsurl = "";
+const womenslippersurl = "";
+const womenflipflopsurl = "";
+const womensandalsurl = "";
+const womenLoafersurl = "";
+const womenMoccasinsurl = "";
+const womenbootsurl = "";
+//
+const kidtopurl = "";
+const kidbottomurl = "";
+const kidfootwareurl = "";
+const kidshoseseurl = "";
+const kidslippersurl = "";
+const kidsandalsurl = "";
+const kidbootssurl = "";
+//
+const menscollectionurl = "";
+const womencollectionurl = "";
+const kidscollectionurl = "";
+const newarrivalurl =
+  "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/694887173992043.649a053632cbd.png";
+const saleurl =
+  "https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/9e88d5166564705.641a70a11e405.png";
